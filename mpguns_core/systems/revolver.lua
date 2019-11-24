4351628fb8931625904ba0b1199e0859e175b99c
RexmecK <rexmeck@gmail.com>
2019-11-24 02:48:31 -0500

    m1911 and Various guns adjustments

--- mpguns_core/systems/revolver.lua
+++ mpguns_core/systems/revolver.lua
@@ -6,13 +6,13 @@ local holding = false
 function main:updateFireControls(dt, firemode, shift, moves)
     --primary firing
     if firemode == "primary" and (self.storage.ammo > 0 or self.storage.loaded == 1) and self.queuedFire == 0 then
-        if not holding and update_lastInfo[2] ~= "primary"  and not animations:isAnyPlaying() then
+        if not holding and update_lastInfo[2] ~= "primary"  and (not animations:isAnyPlaying() or self:isPlaying("fire")) then
             holding = true
             self:animate("ready")
             if self.storage.loaded ~= 1 then
                 self:load()
             end
-        elseif holding and not animations:isAnyPlaying() then
+        elseif holding and (not animations:isAnyPlaying() or (self:isPlaying("fire") and not self:isPlaying("ready"))) then
             holding = false
             if self.config.firemode == "auto" then
                 self.queuedFire = 1
